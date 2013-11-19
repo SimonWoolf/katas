07288151ae000943eb811f4e809d28e99d4a00d2
Simon Woolf <simon@simonwoolf.net>
2013-11-19 17:27:10 +0000

    delicious mathsy oneliners

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,18 @@
 class FizzBuzz
-	def self.fizzify(number)
-		if number % 15 == 0
-			'FizzBuzz'
-		elsif number % 5 == 0
-			'Buzz'
-		elsif number % 3 == 0
-			'Fizz'
-		else
-			number
-		end
+	def self.fizzify(n)
+		div_by_15?(n) ? 'FizzBuzz' : (div_by_5?(n) ? 'Buzz' : (div_by_3?(n) ? 'Fizz' : n)) 
+	end
+
+	def self.div_by_3?(n)
+		# Recursively calculates the digital root of the number until it's one digit long
+		n.to_s.length == 1 ? ([0,3,6,9].include? n) : div_by_3?(n.to_s.split('').map(&:to_i).inject(:+))
+	end
+
+	def self.div_by_5?(n)
+		['0','5'].include? n.to_s[-1]
+	end
+
+	def self.div_by_15?(n)
+		div_by_3?(n) && div_by_5?(n)
 	end
 end
