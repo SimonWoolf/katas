79e2ad87171afaa35251df51a0601347753cd86a
Simon Woolf <simon@simonwoolf.net>
2013-11-22 17:39:51 +0000

    refactor; works up to 99

--- numbers_to_words/number_to_words.rb
+++ numbers_to_words/number_to_words.rb
@@ -1,14 +1,39 @@
 class NumberToWords
-  
-  UNITS = Array(1..10)
-  TEENS = Array(11..19)
-  UNIT_WORDS = %W[one two three four five six seven eight nine ten]
-  TEEN_WORDS = %W[eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen]
-  WORD_MAP = Hash[UNITS.zip(UNIT_WORDS)]
-  TEEN_MAP = Hash[TEENS.zip(TEEN_WORDS)]
 
-  def self.wordify(number)
-    number < 11 ? WORD_MAP[number] : TEEN_MAP[number]
+  def self.mapify(wordarray)
+    Hash[Array(1..9).zip(wordarray)]
   end
 
+  UNITS_WORDS = %w(one two three four five six seven eight nine)
+  TEENS_WORDS = %w(eleven twelve thirteen fourteen fifteen) + 
+               %w(sixteen seventeen eighteen nineteen)
+  TENS_WORDS = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
+  UNITS_MAP = mapify(UNITS_WORDS)
+  TEENS_MAP = mapify(TEENS_WORDS)
+  TENS_MAP = mapify(TENS_WORDS)
+
+  def self.wordify(number)
+    numarray = number.divmod(10)
+    if teen?(number) 
+      TEENS_MAP[numarray[1]]
+    else
+      numarray_to_wordarray(numarray).join(' ').strip
+    end
+  end
+
+  def self.numarray_to_wordarray(numarray)
+    [tens_map(numarray[0]), units_map(numarray[1])]
+  end
+
+  def self.teen?(number)
+    10 < number && number < 20
+  end
+
+  def self.tens_map(number)
+    TENS_MAP[number] || ''
+  end
+
+  def self.units_map(number)
+    UNITS_MAP[number] || ''
+  end
 end
--- numbers_to_words/spec/number_to_words_spec.rb
+++ numbers_to_words/spec/number_to_words_spec.rb
@@ -22,6 +22,26 @@ describe NumberToWords do
       NumberToWords.wordify(11).should == "eleven"
     end
 
+    it '16 into sixteen' do
+      NumberToWords.wordify(16).should == "sixteen"
+    end
+
+    it '19 into nineteen' do
+      NumberToWords.wordify(19).should == "nineteen"
+    end
+
+    it '20 into twenty' do
+      NumberToWords.wordify(20).should == "twenty"
+    end
+
+    it '21 into twenty one' do
+      NumberToWords.wordify(21).should == "twenty one"
+    end
+
+    it '99 into ninety nine' do
+      NumberToWords.wordify(99).should == "ninety nine"
+    end
+
   end
   
 end
