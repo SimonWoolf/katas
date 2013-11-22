69fc06d6793670ecd1ec247230c8ddd6020ee703
Simon Woolf <simon@simonwoolf.net>
2013-11-22 15:52:18 +0000

    works up to nine

--- numbers_to_words/number_to_words.rb
+++ numbers_to_words/number_to_words.rb
@@ -1,2 +1,11 @@
-def number_to_words(n)
+class NumberToWords
+  
+  NUMBERS = Array(1..9)
+  WORDS = %W[one two three four five six seven eight nine]
+  WORD_MAP = Hash[NUMBERS.zip(WORDS)]
+
+  def self.wordify(number)
+    WORD_MAP[number]
+  end
+
 end
