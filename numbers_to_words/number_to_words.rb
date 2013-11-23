c447bd4762effafe8d89e9edd9c20666789a086c
Simon Woolf <simon@simonwoolf.net>
2013-11-23 17:56:44 +0000

    changed tests to work on fixnum directly, got rid of redundant class

--- numbers_to_words/number_to_words.rb
+++ numbers_to_words/number_to_words.rb
@@ -1,73 +1,70 @@
 class Fixnum
   def wordify
-    NumberToWords.wordify(self)
+    number_to_words(self)
   end
 end
 
-class NumberToWords
+def self.mapify(wordarray)
+Hash[Array(1..9).zip(wordarray)]
+end
 
-  def self.mapify(wordarray)
-    Hash[Array(1..9).zip(wordarray)]
-  end
-
-  UNITS_WORDS = %w(one two three four five six seven eight nine)
-  TEENS_WORDS = %w(eleven twelve thirteen fourteen fifteen) + 
-               %w(sixteen seventeen eighteen nineteen)
-  TENS_WORDS = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
-  UNITS_MAP = mapify(UNITS_WORDS)
-  TEENS_MAP = mapify(TEENS_WORDS)
-  TENS_MAP = mapify(TENS_WORDS)
-  class << self
-    def wordify(number)
-      raise 'Can\'t deal with numbers that high' unless number < 1_000_000_000
-      return millions(number).strip
-    end
-
-    def millions(number)
-      millions, remainder = number.divmod(1_000_000)
-      (millions != 0 ? triplets(millions) + ' million ': '') + thousands(remainder)
-    end
-
-    def thousands(number)
-      thousands, remainder = number.divmod(1000)
-      (thousands != 0 ? triplets(thousands) + ' thousand ': '') + triplets(remainder)
-    end
-    
-    def triplets(number)
-      number < 100 ? tensunits(number) : hundreds(number)
-    end
-
-    def hundreds(number)
-      hundreds, tensunits = number.divmod(100)
-      units_map(hundreds) + " hundred" \
-      + (tensunits != 0 ? " and " + tensunits(tensunits) : '') 
-    end
-
-    def tensunits(number)
-      tens, units = number.divmod(10)
-      if teen?(number) 
-        TEENS_MAP[units]
-      else
-        (tens != 0 ? tens_map(tens) + ' ' : '') + units_map(units)
-      end
-    end
-
-    def unit?(numarray)
-      numarray.length == 1
-    end
-
-    def teen?(number)
-      number >= 11 && number <= 19
-    end
-
-    def tens_map(number)
-      TENS_MAP[number] || ''
-    end
-
-    def units_map(number)
-      UNITS_MAP[number] || ''
-    end
+UNITS_WORDS = %w(one two three four five six seven eight nine)
+TEENS_WORDS = %w(eleven twelve thirteen fourteen fifteen) + 
+           %w(sixteen seventeen eighteen nineteen)
+TENS_WORDS = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
+UNITS_MAP = mapify(UNITS_WORDS)
+TEENS_MAP = mapify(TEENS_WORDS)
+TENS_MAP = mapify(TENS_WORDS)
 
 
+def number_to_words(number)
+  raise 'Can\'t deal with numbers that high' unless number < 1_000_000_000
+  return millions(number).strip
+end
+
+def millions(number)
+  millions, remainder = number.divmod(1_000_000)
+  (millions != 0 ? triplets(millions) + ' million ': '') + thousands(remainder)
+end
+
+def thousands(number)
+  thousands, remainder = number.divmod(1000)
+  (thousands != 0 ? triplets(thousands) + ' thousand ': '') + triplets(remainder)
+end
+
+def triplets(number)
+  number < 100 ? tensunits(number) : hundreds(number)
+end
+
+def hundreds(number)
+  hundreds, tensunits = number.divmod(100)
+  units_map(hundreds) + " hundred" \
+  + (tensunits != 0 ? " and " + tensunits(tensunits) : '') 
+end
+
+def tensunits(number)
+  tens, units = number.divmod(10)
+  if teen?(number) 
+    TEENS_MAP[units]
+  else
+    (tens != 0 ? tens_map(tens) + ' ' : '') + units_map(units)
   end
 end
+
+def unit?(numarray)
+  numarray.length == 1
+end
+
+def teen?(number)
+  number >= 11 && number <= 19
+end
+
+def tens_map(number)
+  TENS_MAP[number] || ''
+end
+
+def units_map(number)
+  UNITS_MAP[number] || ''
+end
+
+
--- numbers_to_words/spec/number_to_words_spec.rb
+++ numbers_to_words/spec/number_to_words_spec.rb
@@ -1,83 +1,83 @@
 require_relative '../number_to_words'
 
-describe NumberToWords do
+describe Fixnum do
   context 'it should turn' do
     it '1 into one' do
-      NumberToWords.wordify(1).should == "one"
+      1.wordify.should == "one"
     end
 
     it '2 into two' do
-      NumberToWords.wordify(2).should == "two"
+      2.wordify.should == "two"
     end
 
     it '3 into three' do
-      NumberToWords.wordify(3).should == "three"
+      3.wordify.should == "three"
     end
 
     it '10 into ten' do
-      NumberToWords.wordify(10).should == "ten"
+      10.wordify.should == "ten"
     end
 
     it '11 into eleven' do
-      NumberToWords.wordify(11).should == "eleven"
+      11.wordify.should == "eleven"
     end
 
     it '16 into sixteen' do
-      NumberToWords.wordify(16).should == "sixteen"
+      16.wordify.should == "sixteen"
     end
 
     it '19 into nineteen' do
-      NumberToWords.wordify(19).should == "nineteen"
+      19.wordify.should == "nineteen"
     end
 
     it '20 into twenty' do
-      NumberToWords.wordify(20).should == "twenty"
+      20.wordify.should == "twenty"
     end
 
     it '21 into twenty one' do
-      NumberToWords.wordify(21).should == "twenty one"
+      21.wordify.should == "twenty one"
     end
 
     it '99 into ninety nine' do
-      NumberToWords.wordify(99).should == "ninety nine"
+      99.wordify.should == "ninety nine"
     end
 
     it '100 into one hundred' do
-      NumberToWords.wordify(100).should == "one hundred"
+      100.wordify.should == "one hundred"
     end
 
     it '101 into one hundred and one' do
-      NumberToWords.wordify(101).should == "one hundred and one"
+      101.wordify.should == "one hundred and one"
     end
 
     it '110 into one hundred and ten' do
-      NumberToWords.wordify(110).should == "one hundred and ten"
+      110.wordify.should == "one hundred and ten"
     end
 
     it '199 into one hundred and ninety nine' do
-      NumberToWords.wordify(199).should == "one hundred and ninety nine"
+      199.wordify.should == "one hundred and ninety nine"
     end
 
     example '200' do
-      NumberToWords.wordify(200).should == "two hundred"
+      200.wordify.should == "two hundred"
     end
 
     example '747' do
-      NumberToWords.wordify(747).should == "seven hundred and forty seven"
+      747.wordify.should == "seven hundred and forty seven"
     end
 
     example '1000' do
-      NumberToWords.wordify(1000).should == "one thousand"
+      1000.wordify.should == "one thousand"
     end
 
     example '7125' do
-      NumberToWords.wordify(7125).should == "seven thousand one hundred and twenty five"
+      7125.wordify.should == "seven thousand one hundred and twenty five"
     end
     example '1000000' do
-      NumberToWords.wordify(1000000).should == "one million"
+      1000000.wordify.should == "one million"
     end
     example '9_999_999' do
-      NumberToWords.wordify(9_999_999).should == "nine million nine hundred and ninety nine thousand nine hundred and ninety nine"
+      9_999_999.wordify.should == "nine million nine hundred and ninety nine thousand nine hundred and ninety nine"
     end
 
 
